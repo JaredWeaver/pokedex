c1e112f69ed38a1241517377948784e0908392c2
JaredWeav <jared.weaver2@gmail.com>
2021-08-04 11:54:32 -0400

    migrated hackerrank files created

---
+++ index.js
@@ -0,0 +1,41 @@
+let pokedexList = document.getElementById('pokemon');
+
+const allPokemonURL = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+
+const gottaCatchEmAll = () => {
+  fetch(allPokemonURL)
+    .then((response) => response.json())
+    .then((data) => {
+      const pokemon = data.results.map((data, index) => ({
+        name: data.name,
+        id: index + 1,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+          index + 1
+        }.png`
+      }));
+      appendPokemon(pokemon);
+    });
+};
+
+const appendPokemon = (pokemon) => {
+  const pokemonHTMLString = pokemon
+    .map(
+      (pokeman) =>
+        `<ul><li id="${pokeman.id}" onclick="clickPokemon(${pokeman.id})"><img src="${pokeman.image}"/> <h2>${pokeman.id}. ${pokeman.name}</h2> </a> </li></ul>`
+    )
+    .join('');
+  pokedexList.innerHTML = pokemonHTMLString;
+};
+
+const clickPokemon = (pokemonId) => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      data.abilities.map((ability) => {
+        // add modal with each ability
+      });
+    });
+};
+
+gottaCatchEmAll();
