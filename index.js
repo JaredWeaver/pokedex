a230e0bd488ca1befb9dcbf321954dad5261c91d
JaredWeav <jared.weaver2@gmail.com>
2021-08-14 14:36:29 -0400

    formatting

--- index.js
+++ index.js
@@ -1,44 +1,65 @@
 let pokedexList = document.getElementById('pokemon');
 let modalBtn = document.getElementById('modal-btn');
-let modal = document.querySelector('.modal');
+let modal = document.getElementById('modal');
 let closeBtn = document.querySelector('.close-btn');
+let pokeCard = document.querySelector('.pokeCard');
+let modalContent = document.querySelector('#modalContent');
 
-const allPokemonURL = 'https://pokeapi.co/api/v2/pokemon?limit=151';
 
-const gottaCatchEmAll = () => {
-  fetch(allPokemonURL)
-    .then((response) => response.json())
-    .then((data) => {
-      const pokemon = data.results.map((data, index) => ({
-        name: data.name,
-        id: index + 1,
-        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-          index + 1
-        }.png`
-      }));
-      appendPokemon(pokemon);
-    });
+const gottaCatchEmAll = async () => {
+  const allPokemonURL = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+  const res = await fetch(allPokemonURL);
+  const data = await res.json();
+  const pokemon = data.results.map((data, index) => ({
+    name: data.name,
+    id: index + 1,
+    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+      index + 1
+    }.png`
+  }));
+  appendPokemon(pokemon);
 };
 
 const appendPokemon = (pokemon) => {
   const pokemonHTMLString = pokemon
     .map(
       (pokeman) =>
-        `<ul><li id="${pokeman.id}" onclick="clickPokemon(${pokeman.id})"><img src="${pokeman.image}"/> <h2>${pokeman.id}. ${pokeman.name}</h2> </a> </li></ul>`
+        `<ul><li id="${pokeman.id}" class='pokeModal' onclick="clickPokemon(${pokeman.id})"><img src="${pokeman.image}"/> <h2>${pokeman.id}. ${pokeman.name}</h2> </a> </li></ul>`
     )
     .join('');
   pokedexList.innerHTML = pokemonHTMLString;
 };
 
-const clickPokemon = (pokemonId) => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      const pokemonCard = data.map((attribute) => {
-        ``;
-      });
-    });
+const clickPokemon = async (pokemonId) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+  const res = await fetch(url);
+  const object = await res.json();
+  activateModalCard(object);
+  console.log('object', object);
+};
+
+const activateModalCard = (pokemon) => {
+  modal.style.display = 'block';
+
+  console.log(modal);
+
+  closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+
+  window.addEventListener('click', (e) => {
+    if (e.target == modal) {
+      modal.style.display = 'none';
+    }
+  });
+console.log(pokemon, 'pokemon')
+const pokemonDetails = `<ul>
+  <li class='pokeCard'>
+    <img src="${pokemon.sprites.front_default}"/>
+    <h2>${pokemon.id}. ${pokemon.name}</h2>
+  </li>
+</ul>`
+modalContent.innerHTML = pokemonDetails;
 };
 
 gottaCatchEmAll();
